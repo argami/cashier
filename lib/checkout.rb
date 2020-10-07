cee78dae33497bfdebb2714240ce46cdce29dc4e
Gamaliel Toro <argami@gmail.com>
2020-10-08 00:39:47 +0200

    first implementation, it doesn't apply rules its simple checkout qty * price

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,13 +1,21 @@
 class Checkout
   def initialize(rules = [])
     @rules = rules
+    @items = []
+    @cart = {} 
+    
+    # group quantity set to 0
+    rules.each { |rule| @cart[rule[0]] = 0 } 
   end
 
   def scan(barcode)
-  
+    @items << barcode
+    @cart[barcode] += 1
   end
 
   def total
-    3.11
+    @cart.reduce(0) do |total, (item, quantity)|
+      total += @rules[item][:price] * quantity 
+    end
   end  
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,13 +1,25 @@
-require "checkout.rb"
+require 'checkout.rb'
 
-RSpec.describe Checkout do 
-    describe "simple transaction" do
-        context "without pricing_rules" do
-            it "returns total of £3.11" do
-                co = Checkout.new([])
-                co.scan("GR1")
-                expect(co.total).to eq 3.11
-            end
-        end
+RSpec.describe Checkout do
+  describe 'simple transaction' do
+    before(:each) do
+      @pricing_rules = { 'GR1' => { price: 3.11,
+                                    rule: { min_quantity: 0, discount: 0 } } }
     end
+
+    context 'without pricing_rules' do
+      it 'returns total of £3.11' do
+        co = Checkout.new(@pricing_rules)
+        co.scan('GR1')
+        expect(co.total).to eq 3.11
+      end
+
+      it 'returns total of £6.22' do
+        co = Checkout.new(@pricing_rules)
+        co.scan('GR1')
+        co.scan('GR1')
+        expect(co.total).to eq 6.22
+      end
+    end
+  end
 end
