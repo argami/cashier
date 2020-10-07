0e7e09bcb6d1d81535e917cc7bc8ce934c92bef3
Gamaliel Toro <argami@gmail.com>
2020-10-07 21:54:25 +0200

    first simple test based on the requirements of the interface

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,13 @@
+require "checkout.rb"
+
+RSpec.describe Checkout do 
+    describe "simple transaction" do
+        context "without pricing_rules" do
+            it "returns total of £3.11" do
+                co = Checkout.new([])
+                co.scan("GR1")
+                expect(co.total).to eq '£3.11'
+            end
+        end
+    end
+end
