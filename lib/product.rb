3cc43e318f853defd2b2a3b1692f03a00a5b3b4f
Gamaliel Toro <argami@gmail.com>
2020-10-09 00:10:18 +0200

    validating price and product code

--- lib/product.rb
+++ lib/product.rb
@@ -4,6 +4,9 @@ class Product
   attr_accessor :code, :price
 
   def initialize(code, price = 0.00)
+    raise ArgumentError.new('product needs a valid code') if code.empty?
+    raise ArgumentError.new('price needs to be positive or 0') if price.negative?
+
     self.code = code
     self.price = price
   end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -8,10 +8,23 @@ RSpec.describe Product do
       expect { Product.new }.to raise_error(ArgumentError)
     end
 
+    it 'requires product can not be blank' do
+      expect { Product.new('') }.to raise_error(ArgumentError)
+    end
+
     it 'price is default to 0' do
       product = Product.new('GR1')
       expect(product.code).to eq 'GR1'
       expect(product.price).to eq 0.00
+    end
+
+    it 'fails if price isn\'t 0 or positive value' do
+      expect { Product.new('GR1', -1.00) }.to raise_error(ArgumentError)
+    end
+
+    it 'price has to be 0 or positive value' do
+      product = Product.new('GR1', 1.00)
+      expect(product.price).to eq 1.00
     end
   end
 end
