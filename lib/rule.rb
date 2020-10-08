38fb55166a9d83254b040e7c48967bc7970d6f4f
Gamaliel Toro <argami@gmail.com>
2020-10-09 00:45:20 +0200

    adding rule class

---
+++ lib/rule.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Rule
+  attr_accessor :price, :minimun, :per, :product
+
+  def initialize(product, minimun: 0, per: 1, price: nil)
+    self.product = product
+    self.minimun = minimun
+    self.per = per
+
+    # the price of the rule should be the same of
+    # the product in case it wasn't informed
+    self.price = price || product.price
+  end
+
+  def code
+    product.code
+  end
+end
---
+++ spec/rule_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'product.rb'
+require 'rule.rb'
+
+RSpec.describe Rule do
+  before(:each) do
+    @product = Product.new 'GR1', 3.11
+  end
+
+  describe '#new' do
+    it 'should set up the defaults values ' do
+      rule = Rule.new @product
+      expect(rule.code).to eq @product.code
+      expect(rule.minimun).to eq 0
+      expect(rule.per).to eq 1
+      expect(rule.price).to eq @product.price
+    end
+
+    it 'should set up the defaults values ' do
+      rule = Rule.new @product, minimun: 2, per: 2, price: 4.11
+      expect(rule.code).to eq @product.code
+      expect(rule.minimun).to eq 2
+      expect(rule.per).to eq 2
+      expect(rule.price).to eq 4.11
+    end
+  end
+end
